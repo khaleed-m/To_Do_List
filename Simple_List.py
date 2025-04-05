7d15f695ac9d74d8989ab578c1a5f29c64f90d10
KHALEED\mulla <mullamohammadk123@gmail.com>
2025-04-05 23:00:30 +0530

    Create table if not exists

--- Simple_List.py
+++ Simple_List.py
@@ -2,3 +2,12 @@ import sqlite3
 
 conn=sqlite3.connect("my_database.db")
 cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    age INTEGER NOT NULL
+)
+""")
+conn.commit()
