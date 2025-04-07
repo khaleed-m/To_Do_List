deb9a5d55ec5839d7a9310b41f4194b741c22df0
KHALEED\mulla <mullamohammadk123@gmail.com>
2025-04-07 23:13:59 +0530

    Creating the table

--- TO_Do_List.py
+++ TO_Do_List.py
@@ -1,3 +1,11 @@
 import sqlite3
 conn=sqlite3.connect("tasks.db")
 cursor = conn.cursor()
+
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS tasks(
+    id INTEGER PRIMARY KEY,
+    task TEXT,
+    status TEXT
+)""")
+conn.commit()
