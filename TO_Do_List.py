5001f7a163c7f0a905126f41b8ce5b1afee42a75
KHALEED\mulla <mullamohammadk123@gmail.com>
2025-04-09 21:27:42 +0530

    Simple interactive menu for smooth UX,🔧 Built using Python and SQLite for easy local task management!

--- TO_Do_List.py
+++ TO_Do_List.py
@@ -57,3 +57,30 @@ def delete_task():
         print(f"Task {task_id} deleted successfully!")
     except ValueError:
         print("Invalid TaskID.")
+
+def main():
+    while True:
+        print("\n To-Do List")
+        print("1. Add Task")
+        print("2. View Tasks")
+        print("3. Mark Task as Complete")
+        print("4. Delete Task")
+        print("5. Exit")
+
+        choice=input("Enter choice: ")
+        if choice=="1":
+            add_task()
+        elif choice=="2":
+            view_tasks()
+        elif choice=="3":
+            complete_task()
+        elif choice=="4":
+            delete_task()
+        elif choice=="5":
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please enter the correct choice(1-5)")
+    conn.close()
+if __name__=="__main__":
+    main()
