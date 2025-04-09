c003e969724cee9c2c2f9adc76f3e8371accabb6
KHALEED\mulla <mullamohammadk123@gmail.com>
2025-04-09 21:24:34 +0530

    View all tasks with status

--- TO_Do_List.py
+++ TO_Do_List.py
@@ -26,4 +26,14 @@ def add_task():
         print(f"Task '{task}' added successfully!")
     else:
         print("Task cannot be empty")
-        
+
+
+def view_tasks():
+    cursor.execute("SELECT * FROM tasks")
+    tasks=cursor.fetchall()
+    if tasks:
+        print("\n To-Do List")
+        for i in tasks:
+            print(f"[{i[0]}] [{i[1]}] [{i[2]}]")
+    else:
+        print("\n No tasks found")
